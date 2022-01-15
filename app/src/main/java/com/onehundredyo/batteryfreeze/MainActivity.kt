9e03a314adee86d82d0d1a0db0571e9784393091
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-01-15 16:33:19 +0900

    Delete 사용하지 않는 XML 제거

--- app/src/main/java/com/onehundredyo/batteryfreeze/MainActivity.kt
+++ app/src/main/java/com/onehundredyo/batteryfreeze/MainActivity.kt
@@ -1,38 +1,24 @@
 package com.onehundredyo.batteryfreeze
 
-import android.Manifest
-import android.app.ActivityManager
 import android.app.AppOpsManager
 import android.app.AppOpsManager.MODE_ALLOWED
 import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
-import android.app.usage.UsageStats
-import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.Intent
-import android.net.TrafficStats
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Process
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
-import com.onehundredyo.batteryfreeze.adapter.ProcessViewHolderAdapter
 import java.lang.Thread.sleep
-import java.util.*
-import android.telephony.TelephonyManager
 
 import android.net.ConnectivityManager
 
 import android.app.usage.NetworkStats
-import android.os.RemoteException
 import android.app.usage.NetworkStatsManager
-import android.content.pm.PackageManager
-import android.nfc.Tag
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import java.security.AccessController.getContext
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 
 
@@ -42,8 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.M)
     lateinit var networkStatsManager:NetworkStatsManager
-    lateinit var processViewHolderAdapter: ProcessViewHolderAdapter
-    val datas = mutableListOf<ProcessData>()
+    val datas = mutableListOf<ApplicationUsageData>()
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,12 +64,12 @@ class MainActivity : AppCompatActivity() {
                     receivedWifi = receivedWifi + bucketWifi.rxBytes
                     sentWifi = sentWifi + bucketWifi.txBytes
                 }
+                datas.add(ApplicationUsageData(uid, packageName,receivedWifi + sentWifi))
                 Log.d(TAG +"결과","패키지명: ${packageName} uid: ${uid}  ##wifi: ${receivedWifi + sentWifi}" )
             }
-
         }
+        datas.sortByDescending { it.rxtx }
 
-        initRecycler()
     }
 
     override fun onStart() {
@@ -104,25 +89,10 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
     }
 
-    private fun initRecycler() {
-        sleep(5000)
-        processViewHolderAdapter = ProcessViewHolderAdapter(this)
-        datas.apply {
-            add(ProcessData(name = "Test1", rx = 4545, tx = 45450))
-            add(ProcessData(name = "Test2", rx = 4545, tx = 45450))
-            add(ProcessData(name = "Test3", rx = 4545, tx = 45450))
-        }
-        processViewHolderAdapter.datas = datas
-        processViewHolderAdapter.notifyDataSetChanged()
-    }
-
     private fun checkForPermission(): Boolean {
         val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
         val mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
         return mode == MODE_ALLOWED
     }
-
-
-
 
 }
--- app/src/main/java/com/onehundredyo/batteryfreeze/ProcessData.kt
+++
@@ -1,7 +0,0 @@
-package com.onehundredyo.batteryfreeze
-
-data class ProcessData(
-    var name: String,
-    val rx: Long,
-    val tx: Long
-)
