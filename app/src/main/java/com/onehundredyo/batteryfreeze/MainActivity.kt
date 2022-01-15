67c28699fefb563213da7ded369ebab9f5d39a72
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-01-15 17:59:43 +0900

    MainActivity의 데이터 사용량 확인 코드 리팩토링

--- app/src/main/java/com/onehundredyo/batteryfreeze/MainActivity.kt
+++ app/src/main/java/com/onehundredyo/batteryfreeze/MainActivity.kt
@@ -20,6 +20,7 @@ import android.app.usage.NetworkStatsManager
 import android.os.Build
 import androidx.annotation.RequiresApi
 import android.content.pm.PackageInfo
+import java.util.*
 
 
 val TAG = "Test"
@@ -27,7 +28,7 @@ val TAG = "Test"
 class MainActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.M)
-    lateinit var networkStatsManager:NetworkStatsManager
+    lateinit var networkStatsManager: NetworkStatsManager
     val datas = mutableListOf<ApplicationUsageData>()
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -39,37 +40,9 @@ class MainActivity : AppCompatActivity() {
             applicationContext.getSystemService(NETWORK_STATS_SERVICE) as NetworkStatsManager
         val packageManager = applicationContext.packageManager
         // 설치된 어플 목록
-        var list:List<PackageInfo> = packageManager.getInstalledPackages(0)
-        for(i in list.indices){
-            var packageName = list.get(i).packageName
-//                Log.d(TAG +"리스트", list.get(i).packageName)
-            if(!packageName.contains("com.samsung") and !packageName.contains("com.android"))
-            {
-                var info = packageManager.getApplicationInfo(packageName, 0)
-                var uid = info.uid
-                val nwStatsWifi = networkStatsManager.queryDetailsForUid(
-                    ConnectivityManager.TYPE_WIFI,
-                    null,
-                    System.currentTimeMillis() - 1296000000,
-                    System.currentTimeMillis(),
-                    uid
-                )
-
-                val bucketWifi = NetworkStats.Bucket()
-                var receivedWifi = 0L
-                var sentWifi = 0L
-
-                while (nwStatsWifi.hasNextBucket()) {
-                    nwStatsWifi.getNextBucket(bucketWifi)
-                    receivedWifi = receivedWifi + bucketWifi.rxBytes
-                    sentWifi = sentWifi + bucketWifi.txBytes
-                }
-                datas.add(ApplicationUsageData(uid, packageName,receivedWifi + sentWifi))
-                Log.d(TAG +"결과","패키지명: ${packageName} uid: ${uid}  ##wifi: ${receivedWifi + sentWifi}" )
-            }
-        }
-        datas.sortByDescending { it.rxtx }
-
+        var list: List<PackageInfo> = packageManager.getInstalledPackages(0)
+        getDataUsageWithPeriod(list, 1, 0)     // getDataUsageWithPeriod(list, time, type)
+        datas.sortByDescending { it.rxtx }       // 데이터 사용량 기준 정렬
     }
 
     override fun onStart() {
@@ -95,4 +68,85 @@ class MainActivity : AppCompatActivity() {
         return mode == MODE_ALLOWED
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun getDataUsageWithPeriod(list: List<PackageInfo>, time: Int, type: Int) {
+        // Time 1은 오늘 00시부터
+        // Time 2는 이번주 월요일 00시부터
+        // Time 3은 이번달 1일 00시부터
+        // Time 4는 올해 1월 1일 00시부터
+
+        // type 0은 데이터
+        // type 1는 와이파이
+
+        var start_time = 0L
+        if (time == 1) {
+            // 일간
+            start_time = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }.time.time
+
+        } else if (time == 2) {
+            // 주간
+            start_time = Calendar.getInstance().apply {
+                set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }.time.time
+        } else if (time == 3) {
+            // 월간
+            start_time = Calendar.getInstance().apply {
+                set(Calendar.DATE, 1)
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }.time.time
+
+        } else if (time == 4) {
+            // 연간
+            start_time = Calendar.getInstance().apply {
+                set(Calendar.DAY_OF_YEAR, 1)
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }.time.time
+        } else {
+            Log.d(TAG, "getDataUsageWithPeriod TIME 설정이 잘못되었습니다")
+        }
+        for (i in list.indices) {
+            var packageName = list.get(i).packageName
+            if (!packageName.contains("com.samsung") and !packageName.contains("com.android")) {
+                var info = packageManager.getApplicationInfo(packageName, 0)
+                var uid = info.uid
+                val nwStatsWifi = networkStatsManager.queryDetailsForUid(
+                    type,
+                    null,
+                    start_time,
+                    System.currentTimeMillis(),
+                    uid
+                )
+
+                val bucketWifi = NetworkStats.Bucket()
+                var receivedWifi = 0L
+                var sentWifi = 0L
+
+                while (nwStatsWifi.hasNextBucket()) {
+                    nwStatsWifi.getNextBucket(bucketWifi)
+                    receivedWifi +=  bucketWifi.rxBytes
+                    sentWifi += bucketWifi.txBytes
+                }
+                datas.add(ApplicationUsageData(uid, packageName, receivedWifi + sentWifi))
+                Log.d(
+                    TAG + "결과",
+                    "패키지명: ${packageName} uid: ${uid}  ##usage: ${receivedWifi + sentWifi}"
+                )
+            }
+        }
+    }
 }
