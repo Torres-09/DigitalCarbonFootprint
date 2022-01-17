2d77a527088a354072fc12c78dce78d34974dbbe
dla280 <dla612@naver.com>
2022-01-17 23:12:14 +0900

    BarChart index labeling

--- app/src/main/java/com/onehundredyo/batteryfreeze/fragment/StatisticsFragment.kt
+++ app/src/main/java/com/onehundredyo/batteryfreeze/fragment/StatisticsFragment.kt
@@ -1,8 +1,10 @@
 package com.onehundredyo.batteryfreeze.fragment
 
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.*
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +12,12 @@ import android.view.ViewGroup
 import com.example.myapplication.DataUsage
 import com.github.mikephil.charting.animation.ChartAnimator
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.interfaces.dataprovider.BarDataProvider
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.renderer.BarChartRenderer
@@ -35,138 +39,151 @@ class StaticFragment : Fragment() {
     private var DataList = ArrayList<DataUsage>()
     private lateinit var barChart: BarChart
     lateinit var mainActivity: MainActivity     // CONTEXT
-    var yearlyData :MutableList<YearlyInfo> = mutableListOf()
-    var monthlyData: MutableList<MonthlyInfo>  = mutableListOf()
-    var weeklyData: MutableList<WeeklyInfo>  = mutableListOf()
+    var yearlyData: MutableList<YearlyInfo> = mutableListOf()
+    var monthlyData: MutableList<MonthlyInfo> = mutableListOf()
+    var weeklyData: MutableList<WeeklyInfo> = mutableListOf()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-         mainActivity = context as MainActivity
+        mainActivity = context as MainActivity
         // DB를 사용하기 위해 CONTEXT 를 얻어옴
     }
 
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            getDatabase()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getDatabase()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistics, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        barChart = view.findViewById(R.id.barchart)
+        DataList = getBarDataUsage()
+
+        initBarChart()
+
+        val entries: ArrayList<BarEntry> = ArrayList()
+
+        for (i in DataList.indices) {
+            val dataUsage = DataList[i]
+            entries.add(BarEntry(i.toFloat(), dataUsage.datausage.toFloat()))
         }
 
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_statistics, container, false)
-        }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
+        // label 이름이랑 color 설정 - barchart
+        val barDataSet = BarDataSet(entries, "")
+        barDataSet.color = ColorTemplate.rgb("#5C98AF")
+        val data = BarData(barDataSet)
+        data.barWidth = 0.35f
+        barChart.data = data
 
-            barChart = view.findViewById(R.id.barchart)
-            DataList = getBarDataUsage()
+        // shape 둥글게 변경 - barchart
+        val myradius = 40
+        val barChartRender =
+            CustomBarChartRender(barChart, barChart.animator, barChart.viewPortHandler)
+        barChartRender.setRadius(myradius)
+        barChart.renderer = barChartRender
 
-            initBarChart()
+        barChart.invalidate()
+    }
 
-            val entries: ArrayList<BarEntry> = ArrayList()
+    fun getBarDataUsage(): ArrayList<DataUsage> {
 
-            for (i in DataList.indices) {
-                val dataUsage = DataList[i]
-                entries.add(BarEntry(i.toFloat(), dataUsage.datausage.toFloat()))
+        DataList.add(DataUsage("3주전", monthlyData[0].DataUsage))
+        DataList.add(DataUsage("2주전", monthlyData[1].DataUsage))
+        DataList.add(DataUsage("1주전", monthlyData[2].DataUsage))
+        DataList.add(DataUsage("이번주", monthlyData[3].DataUsage))
+
+        return DataList
+    }
+
+    private fun initBarChart() {
+
+        barChart.run {
+            // 막대 그래프 그림자 on
+            setDrawBarShadow(true)
+            // 차트 터치 X
+            setTouchEnabled(false)
+            // 줌 금지
+            setPinchZoom(false)
+
+            // 막대 그래프 올라가는 애니메이션 추가
+            animateXY(0, 800)
+
+            axisLeft.run {
+                // 좌측 y축 제거
+                isEnabled = false
+
+
+            }
+            axisRight.run {
+                //우측 y축 제거
+                isEnabled = false
             }
 
+            xAxis.run {
+                // 막대 그래프 바 grid 제거
+                setDrawGridLines(false)
+                setDrawAxisLine(false)
 
-            // label 이름이랑 color 설정 - barchart
-            val barDataSet = BarDataSet(entries, "")
-            barDataSet.color = ColorTemplate.rgb("#5C98AF")
-            val data = BarData(barDataSet)
-            data.barWidth = 0.35f
-            barChart.data = data
+                // 막대 그래프 설정
+                position = XAxis.XAxisPosition.BOTTOM
+                textColor = ColorTemplate.rgb("#5C98AF")
+                valueFormatter = MyAxisFormatter()
+                granularity = 1f
+                labelRotationAngle = -25f
+            }
 
-            // shape 둥글게 변경 - barchart
-            val myradius = 40
-            val barChartRender =
-                CustomBarChartRender(barChart, barChart.animator, barChart.viewPortHandler)
-            barChartRender.setRadius(myradius)
-            barChart.renderer = barChartRender
+            legend.run {
+                // 하단 항목 이름 제거
+                isEnabled = false
+            }
 
-            barChart.invalidate()
-        }
+            description.run {
+                // 설명 제거
+                isEnabled = false
 
-        fun getBarDataUsage(): ArrayList<DataUsage> {
-
-            DataList.add(DataUsage("3주전", monthlyData[0].DataUsage))
-            DataList.add(DataUsage("2주전", monthlyData[1].DataUsage))
-            DataList.add(DataUsage("1주전", monthlyData[2].DataUsage))
-            DataList.add(DataUsage("이번주", monthlyData[3].DataUsage))
-            DataList.add(DataUsage("이번주", monthlyData[3].DataUsage))
-
-            return DataList
-        }
-
-        private fun initBarChart() {
-
-            barChart.run {
-                // 막대 그래프 그림자 on
-                setDrawBarShadow(true)
-                // 차트 터치 X
-                setTouchEnabled(false)
-                // 줌 금지
-                setPinchZoom(false)
-
-                // 막대 그래프 올라가는 애니메이션 추가
-                animateXY(0, 800)
-
-                axisLeft.run {
-                    // 좌측 y축 제거
-                    isEnabled = false
-
-
-                }
-                axisRight.run {
-                    //우측 y축 제거
-                    isEnabled = false
-                }
-
-                xAxis.run {
-                    // 막대 그래프 바 grid 제거
-                    setDrawGridLines(false)
-                    setDrawAxisLine(false)
-
-                    // 막대 그래프 설정
-                    position = XAxis.XAxisPosition.BOTTOM
-                    textColor = ColorTemplate.rgb("#5C98AF")
-
-                    granularity = 1f
-                    labelRotationAngle = -25f
-                }
-
-                legend.run {
-                    // 하단 항목 이름 제거
-                    isEnabled = false
-                }
-
-                description.run {
-                    // 설명 제거
-                    isEnabled = false
-
-                }
             }
         }
+    }
 
-        private fun getDatabase() {
-            val db = DataBaseManager.getInstance(mainActivity)!!
-            CoroutineScope(Dispatchers.IO).launch {
-                for( i in (db!!. DatausageDAO().getAllYearlyData())){
-                    yearlyData.add(i)
-                }
-                for(i in  db!!.DatausageDAO().getAllMonthlyData()){
-                    monthlyData.add(i)
-                }
-                for( i in db!!.DatausageDAO().getAllWeeklyData()){
-                    weeklyData.add(i)
-                }
+    // 막대 그래프 내부에 항목이름을 각각 적기 - barchart
+    inner class MyAxisFormatter : IndexAxisValueFormatter() {
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            val index = value.toInt()
+            Log.d(ContentValues.TAG, "getAxisLabel: index $index")
+            return if (index < DataList.size) {
+                DataList[index].title
+            } else {
+                ""
             }
         }
+    }
+
+
+    private fun getDatabase() {
+        val db = DataBaseManager.getInstance(mainActivity)!!
+        CoroutineScope(Dispatchers.IO).launch {
+            for (i in (db!!.DatausageDAO().getAllYearlyData())) {
+                yearlyData.add(i)
+            }
+            for (i in db!!.DatausageDAO().getAllMonthlyData()) {
+                monthlyData.add(i)
+            }
+            for (i in db!!.DatausageDAO().getAllWeeklyData()) {
+                weeklyData.add(i)
+            }
+        }
+    }
 
     // change barchart shape circular - barchart
 
