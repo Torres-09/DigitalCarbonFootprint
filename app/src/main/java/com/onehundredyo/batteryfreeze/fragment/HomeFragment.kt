9f06e41ce53232a00d7dbe09de83f13117a53ff3
BBSSJJ <bdhbsj@naver.com>
2022-01-18 23:42:20 +0900

    Feat. 말풍선 추가, 북극곰 클릭 시 단계별 대사 랜덤으로 출력 / Fix. 오늘의 문구 없애고 앱 들어갈 때 마다 랜덤 문구 출력

--- app/src/main/java/com/onehundredyo/batteryfreeze/fragment/HomeFragment.kt
+++ app/src/main/java/com/onehundredyo/batteryfreeze/fragment/HomeFragment.kt
@@ -14,19 +14,10 @@ import com.onehundredyo.batteryfreeze.App
 import java.time.LocalDate
 import java.util.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -38,7 +29,17 @@ class HomeFragment : Fragment() {
             return false
         } else
             return true
+    }
 
+    fun setChatText(view: View, level: Int) {
+        val chatBubbleText: TextView = view.findViewById(R.id.chatBubbleText)
+        var randomNumber = (0..2).random()
+        when (level) {
+            3 -> chatBubbleText.setText(resources.getStringArray(R.array.chat_list3)[randomNumber])
+            2 -> chatBubbleText.setText(resources.getStringArray(R.array.chat_list2)[randomNumber])
+            1 -> chatBubbleText.setText(resources.getStringArray(R.array.chat_list1)[randomNumber])
+            0 -> chatBubbleText.setText(resources.getStringArray(R.array.chat_list0)[randomNumber])
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,16 +59,6 @@ class HomeFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
                 arguments = Bundle().apply {
@@ -83,26 +74,36 @@ class HomeFragment : Fragment() {
         val glacierImage: ImageView = view.findViewById(R.id.glacierImage)
         val polarBearImage: ImageView = view.findViewById(R.id.polarBearImage)
         val remainText: TextView = view.findViewById(R.id.remainText)
+        val chatBubbleText: TextView = view.findViewById(R.id.chatBubbleText)
+        val chatBubble: ImageView = view.findViewById(R.id.chatBubble)
         val animation = AnimationUtils.loadAnimation(activity, R.anim.moving)
-
+        val todayGoalText: TextView = view.findViewById(R.id.todayGoalText)
 
         //퍼센트에 따른 변화
+        var randomNumber = (0..2).random()
+        lateinit var textArray: Array<String>
         var remainPercentage = 27
         when (remainPercentage) {
-            in 76..100 -> {}
+            in 76..100 -> {
+                polarBearImage.setOnClickListener { setChatText(view, 3) }
+            }
             in 51..75 -> {
+                polarBearImage.setOnClickListener { setChatText(view, 2) }
+
 //                glacierImage.setImageResource(R.drawable.glacier0)
 //                polarBearImage.setImageResource(R.drawable.polar_bear2)
 //                glacierImage.startAnimation(animation)
 //                polarBearImage.startAnimation(animation)
             }
             in 26..50 -> {
+                polarBearImage.setOnClickListener { setChatText(view, 1) }
+                textArray = resources.getStringArray(R.array.chat_list1)
                 glacierImage.setImageResource(R.drawable.glacier0)
                 polarBearImage.setImageResource(R.drawable.polar_bear1)
-                glacierImage.startAnimation(animation)
-                polarBearImage.startAnimation(animation)
             }
             in 0..25 -> {
+                polarBearImage.setOnClickListener { setChatText(view, 0) }
+                textArray = resources.getStringArray(R.array.chat_list0)
 //                glacierImage.setImageResource(0)
 //                polarBearImage.setImageResource(0)
             }
@@ -110,19 +111,27 @@ class HomeFragment : Fragment() {
             }
         }
         remainText.setText("남은 목표량: $remainPercentage%")
+        glacierImage.startAnimation(animation)
+        polarBearImage.startAnimation(animation)
+        chatBubble.startAnimation(animation)
+        chatBubbleText.startAnimation(animation)
 
+        //문구
+//        val todayGoalText: TextView = view.findViewById(R.id.todayGoalText)
+//        if (compareDate()) {
+//            todayGoalText.setText(App.prefs.getSavedText("savedDate", "error"))
+//        } else {
+//            val range = (0..6)
+//            val randomNumber = range.random()
+//            val textArray: Array<String> = resources.getStringArray(R.array.dailyMission)
+//            val newText: String = textArray[randomNumber]
+//            todayGoalText.setText(newText)
+//            App.prefs.setSavedText("savedDate", newText)
+//        }
 
-        //오늘의 문구
-        val todayGoalText: TextView = view.findViewById(R.id.todayGoalText)
-        if (compareDate()) {
-            todayGoalText.setText(App.prefs.getSavedText("savedDate", "error"))
-        } else {
-            val range = (0..6)
-            val randomNumber = range.random()
-            val textArray: Array<String> = resources.getStringArray(R.array.dailyMission)
-            val newText: String = textArray[randomNumber]
-            todayGoalText.setText(newText)
-            App.prefs.setSavedText("savedDate", newText)
-        }
+        //문구
+        randomNumber = (0..6).random()
+        textArray = resources.getStringArray(R.array.dailyMission)
+        todayGoalText.setText(textArray[randomNumber])
     }
 }
