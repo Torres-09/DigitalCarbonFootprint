b7e8bc49ad19232124ba4a2c58f910a7c48510c8
dla280 <dla612@naver.com>
2022-01-19 03:36:34 +0900

    onCreate제거

--- app/src/main/java/com/onehundredyo/batteryfreeze/fragment/HomeFragment.kt
+++ app/src/main/java/com/onehundredyo/batteryfreeze/fragment/HomeFragment.kt
@@ -50,14 +50,6 @@ class HomeFragment : Fragment() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
