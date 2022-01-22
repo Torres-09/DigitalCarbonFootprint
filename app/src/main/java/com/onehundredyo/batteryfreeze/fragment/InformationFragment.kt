09d5d5bdb620f04a5853970d5d362ab0d1e8f3a4
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-01-22 14:27:22 +0900

    Feat.펀딩페이지 완성

--- app/src/main/java/com/onehundredyo/batteryfreeze/fragment/InformationFragment.kt
+++ app/src/main/java/com/onehundredyo/batteryfreeze/fragment/InformationFragment.kt
@@ -1,24 +1,27 @@
 package com.onehundredyo.batteryfreeze.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
-import androidx.recyclerview.widget.RecyclerView
-import com.onehundredyo.batteryfreeze.DO.News
 import com.onehundredyo.batteryfreeze.R
-import com.onehundredyo.batteryfreeze.adapter.CustomListViewAdapter
 
 class InformationFragment : Fragment() {
-    private lateinit var customListviewAdapter: CustomListViewAdapter
-    private lateinit var news_list : MutableList<News>
-    private lateinit var RecycleViewCustom: RecyclerView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var fundimage: ImageView
+    private lateinit var fundimagedetail: ImageView
+    private lateinit var fadeInAnim: Animation
+    private lateinit var fadeOutAnim: Animation
+    private lateinit var mContext: Context
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext=context
     }
 
     override fun onCreateView(
@@ -28,6 +31,36 @@ class InformationFragment : Fragment() {
 
         return inflater.inflate(R.layout.fragment_information, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fundimagedetail = view.findViewById(R.id.fund_detail)
+        fundimage = view.findViewById(R.id.fund2)
+
+        fundimagedetail.visibility = View.INVISIBLE
+        fundimagedetail.visibility = View.GONE
+
+        fundimage.visibility = View.VISIBLE
+
+        fadeInAnim = AnimationUtils.loadAnimation(mContext, R.anim.fade_in)
+        fadeOutAnim = AnimationUtils.loadAnimation(mContext, R.anim.fade_out)
+
+        fundimage.setOnClickListener {
+            fundimage.visibility = View.GONE
+            fundimage.visibility = View.INVISIBLE
+            fundimage.startAnimation(fadeOutAnim)
+            fundimagedetail.startAnimation(fadeInAnim)
+            fundimagedetail.visibility = View.VISIBLE
+        }
+        fundimagedetail.setOnClickListener {
+            fundimagedetail.visibility = View.GONE
+            fundimagedetail.visibility = View.INVISIBLE
+            fundimagedetail.startAnimation(fadeOutAnim)
+            fundimage.startAnimation(fadeInAnim)
+            fundimage.visibility = View.VISIBLE
+        }
+    }
+
 
 }
 
